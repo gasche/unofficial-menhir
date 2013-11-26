beb0300e74018a7d65fb107a9a732b3de1ec6b5f
Frederic Bour <frederic.bour@lakaban.net>
2013-11-26 15:58:28 +0100

    Purify: remove side-effects from table-backend generated code

--- src/referenceInterpreter.ml
+++ src/referenceInterpreter.ml
@@ -83,7 +83,7 @@ module T = struct
   exception Error
 
   type semantic_action =
-      (state, semantic_value, token) env -> unit
+      (state, semantic_value, token) env -> (state, semantic_value) stack
 
   let semantic_action (prod : production) : semantic_action =
     fun env ->
@@ -156,7 +156,7 @@ module T = struct
           (* Construct and push a new stack cell. The associated semantic
              value is a new concrete syntax tree. *)
 
-          env.stack <- {
+          {
             state = env.current;
             semv = CstNonTerminal (prod, values);
             startp = !startp;
