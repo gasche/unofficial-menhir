bfd1021ccffdc306fb865e3d48371661a32aea02
Frederic Bour <frederic.bour@lakaban.net>
2013-12-01 18:15:31 +0100

    Settings: add --stepwise flag

--- src/error.ml
+++ src/error.ml
@@ -110,3 +110,8 @@ let grammar_warning positions message =
   else
     warning positions message
 
+(* Check consistency of selected settings. *)
+
+let () =
+  if Settings.stepwise && not Settings.table then
+    error [] "--stepwise requires --table backend"
