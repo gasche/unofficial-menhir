let-def/menhir

Interface/TableBackend: generate stepwise engine

src/interface.ml
@@ -43,6 +43,79 @@ let entrytypescheme symbol =
   in
   type2scheme (marrow [ arrow tlexbuf ttoken; tlexbuf ] (TypTextual ocamltype))
 
+let entryvaldecls =
+  StringSet.fold (fun symbol decls ->
+      (Misc.normalize symbol, entrytypescheme symbol) :: decls
+    ) PreFront.grammar.start_symbols []
+
+(* Type definitions of stepwise interface *)
+
+let steptypedef =
+  let tyenv = TypApp ("MenhirLib.EngineTypes.env", [
+      TypApp ("state",[]); TypApp ("semantic_value",[]); TypApp ("token",[]);
+    ])
+  in
+  [
+    { typename = "state"; typerhs = TDefSum [];
+      typeparams = []; typeconstraint = None; typeprivate = false };
+    { typename = "semantic_value";
+      typerhs = TAbbrev (TypApp ("Obj.t", []));
+      typeparams = []; typeconstraint = None; typeprivate = false };
+    { typename = "step"; typeprivate = true;
+      typerhs = TDefSum [
+          { dataname = "Step_run";
+            datavalparams = [tyenv];
+            datatypeparams = None
+          };
+          { dataname = "Step_error";
+            datavalparams = [tyenv];
+            datatypeparams = None
+          };
+          { dataname = "Step_action";
+            datavalparams = [tyenv];
+            datatypeparams = None
+          };
+        ];
+      typeparams = []; typeconstraint = None };
+    { typename = "outcome"; typeprivate = false;
+      typerhs = TDefSum [
+          { dataname = "Step";
+            datavalparams = [TypApp ("step",[])];
+            datatypeparams = None
+          };
+          { dataname = "Accept";
+            datavalparams = [TypApp ("semantic_value",[])];
+            datatypeparams = None
+          };
+          { dataname = "Reject";
+            datavalparams = [];
+            datatypeparams = None
+          };
+          { dataname = "Feed";
+            datavalparams = [TypArrow (TypTuple [
+                TypApp ("Lexing.position",[]);
+                TypApp ("token",[]);
+                TypApp ("Lexing.position",[]);
+              ], TypApp ("step",[]))];
+            datatypeparams = None
+          };
+        ];
+      typeparams = []; typeconstraint = None };
+  ]
+
+let stepvaldecls = [
+  "initial", { quantifiers = [];
+               body = TypArrow (TypApp ("state",[]), TypApp ("outcome",[])) };
+  "step", { quantifiers = [];
+            body = TypArrow (TypApp ("step",[]), TypApp ("outcome",[])) };
+]
+
+let stepentryvaldecls =
+  StringSet.fold (fun symbol decls ->
+      (Misc.normalize symbol ^ "_state",
+       {quantifiers = []; body = TypApp ("state",[])}) :: decls
+    ) PreFront.grammar.start_symbols []
+
 (* This is the interface of the generated parser. *)
 
 let interface = {
@@ -54,14 +127,14 @@ let interface = {
     [ excdef ];
 
   typedecls =
-    tokentypedef;
+    tokentypedef @
+      (if Settings.stepwise then steptypedef else []);
 
   valdecls =
-    StringSet.fold (fun symbol decls ->
-      (Misc.normalize symbol, entrytypescheme symbol) :: decls
-    ) PreFront.grammar.start_symbols []
+    entryvaldecls @
+      (if Settings.stepwise then stepvaldecls @ stepentryvaldecls else []);
 
-} 
+}
 
 (* Writing the interface to a file. *)
 

gasche/unofficial-menhir

src/interface.ml
@@ -31,6 +31,79 @@ let entrytypescheme symbol =
   in
   type2scheme (marrow [ arrow tlexbuf ttoken; tlexbuf ] (TypTextual ocamltype))
 
+let entryvaldecls =
+  StringSet.fold (fun symbol decls ->
+      (Misc.normalize symbol, entrytypescheme symbol) :: decls
+    ) PreFront.grammar.start_symbols []
+
+(* Type definitions of stepwise interface *)
+
+let steptypedef =
+  let tyenv = TypApp ("MenhirLib.EngineTypes.env", [
+      TypApp ("state",[]); TypApp ("semantic_value",[]); TypApp ("token",[]);
+    ])
+  in
+  [
+    { typename = "state"; typerhs = TDefSum [];
+      typeparams = []; typeconstraint = None; typeprivate = false };
+    { typename = "semantic_value";
+      typerhs = TAbbrev (TypApp ("Obj.t", []));
+      typeparams = []; typeconstraint = None; typeprivate = false };
+    { typename = "step"; typeprivate = true;
+      typerhs = TDefSum [
+          { dataname = "Step_run";
+            datavalparams = [tyenv];
+            datatypeparams = None
+          };
+          { dataname = "Step_error";
+            datavalparams = [tyenv];
+            datatypeparams = None
+          };
+          { dataname = "Step_action";
+            datavalparams = [tyenv];
+            datatypeparams = None
+          };
+        ];
+      typeparams = []; typeconstraint = None };
+    { typename = "parser"; typeprivate = false;
+      typerhs = TDefSum [
+          { dataname = "Step";
+            datavalparams = [TypApp ("step",[])];
+            datatypeparams = None
+          };
+          { dataname = "Accept";
+            datavalparams = [TypApp ("semantic_value",[])];
+            datatypeparams = None
+          };
+          { dataname = "Reject";
+            datavalparams = [];
+            datatypeparams = None
+          };
+          { dataname = "Feed";
+            datavalparams = [TypArrow (TypTuple [
+                TypApp ("Lexing.position",[]);
+                TypApp ("token",[]);
+                TypApp ("Lexing.position",[]);
+              ], TypApp ("step",[]))];
+            datatypeparams = None
+          };
+        ];
+      typeparams = []; typeconstraint = None };
+  ]
+
+let stepvaldecls = [
+  "initial", { quantifiers = [];
+               body = TypArrow (TypApp ("state",[]), TypApp ("parser",[])) };
+  "step", { quantifiers = [];
+            body = TypArrow (TypApp ("step",[]), TypApp ("parser",[])) };
+]
+
+let stepentryvaldecls =
+  StringSet.fold (fun symbol decls ->
+      (Misc.normalize symbol ^ "_state",
+       {quantifiers = []; body = TypApp ("state",[])}) :: decls
+    ) PreFront.grammar.start_symbols []
+
 (* This is the interface of the generated parser. *)
 
 let interface = {
@@ -42,12 +115,12 @@ let interface = {
     [ excdef ];
 
   typedecls =
-    tokentypedef;
+    tokentypedef @
+      (if Settings.stepwise then steptypedef else []);
 
   valdecls =
-    StringSet.fold (fun symbol decls ->
-      (Misc.normalize symbol, entrytypescheme symbol) :: decls
-    ) PreFront.grammar.start_symbols [];
+    entryvaldecls @
+      (if Settings.stepwise then stepvaldecls @ stepentryvaldecls else []);
 
   moddecls =
     [];